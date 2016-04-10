0a0c8c036a235249230f4364d3e935e9dffd4347
Dan Keder <dan.keder@gmail.com>
2016-04-10 14:21:19 +0200

    add pg_dump_sample.go

---
+++ README.md
@@ -0,0 +1,148 @@
+`pg_dump_sample`
+===============
+
+This is a simple tool for dumping a sample of data from a PostgreSQL database.
+The resulting dump can be loaded back into an new database using standard tools
+(e.g. `psql(1)`).
+
+
+
+Why would I want this?
+----------------------
+
+It is useful if you have a huge PostgreSQL database and you need a
+database with a small dataset for testing or development.
+
+
+Features
+--------
+
+- Data sampling: Dump either all rows of a table or only rows matching the
+  specified query.
+- Easy anonymization of data: You have full control over what's in the dump,
+  it's very easy to create anonymized DB dumps.
+- Templated queries: You can extract common parts of queries into one place.
+  That's especially useful if you have e.g. a list of IDs the dump should contain.
+- Table dependency awareness - Table A will be dumped before table B
+  automatically if table B has foreign keys pointing to table A.
+- Standard SQL - The produced dump files contain just a bunch of SQL commands.
+  They can be loaded by standard tools such as `psql(1)` or processed further.
+
+
+How to compile/install
+----------------------
+
+The `pg_dump_sample` is written in Go. You need to [setup the Go compiler and
+setup environment](https://golang.org/doc/install) first. Then run:
+
+    go get github.com/dankeder/pg_dump_sample
+
+If it went well you should be able to run it:
+
+    pg_dump_sample
+
+If not check that you have `$GOPATH/bin` in your `$PATH`.
+
+
+How to use
+----------
+
+A quick example:
+
+    pg_dump_sample -m mydb.yaml -h mydbhost.dev -U postgres -f mydb_dump.sql mydb
+
+Available command-line options:
+
+    Usage:
+      pg_dump_sample [options] database
+
+    Application Options:
+      -h, --host=          database server host or socket directory (default: local socket)
+      -p, --port=          database server port (default: 5432)
+      -U, --username=      database user name (default: current user)
+      -w, --no-password    never prompt for password
+      -m, --manifest-file= path to manifest file
+      -f, --file=          path to output file
+          --help           show help
+
+The available command-line options are heavily inspired by
+[`pg_dump(1)`](http://www.postgresql.org/docs/9.4/static/app-pgdump.html).
+Anyone familiar with it should feel right at home.
+
+The main difference from `pg_dump(1)` is that `pg_dump_sample` requires a
+manifest file describing how to dump the database.
+
+
+### Manifest file
+
+The manifest file is a YAML file describing what tables to dump and how to dump
+them.
+
+A quick example:
+
+    ---
+    vars:
+      # Condition to dump only certain users
+      matching_user_id: "(users.id BETWEEN 1000 AND 2000)"
+
+    tables:
+      # Dump everything from table "consts"
+      - table: consts
+
+      # Dump only matching users
+      - table: users
+        query: "SELECT * FROM users WHERE {{matching_user_id}}"
+        post_actions:
+          - "SELECT pg_catalog.setval('users_id_seq', MAX(id) + 1, true) FROM users"
+
+      # Dump only tickets that were bought by matching users
+      - table: tickets
+        query: >
+          SELECT purchases.* FROM purchases, users
+          WHERE
+            purchases.buyer_id = users.id
+            AND {{matching_user_id}}
+
+
+Currently these top-level keys are available:
+
+#### `vars`
+
+Definitions of variables which will be used to replace placeholders in queries.
+
+#### `tables`
+
+List of tables to dump. Tables are dumped in the order they are specified in the
+manifest file, with one exception: if the table contains foreign keys
+referencing another table, the referenced table will be dumped first. This is to
+ensure that the dump can be loaded later without errors.
+
+By default all rows of the table will be dumped. If you don't want to dump all
+the rows use the `query` to specify a SELECT SQL statement which returns the
+rows you want to dump.
+
+
+TODO
+----
+
+- Use separate vars files to override vars from manifest?
+- Allow setting vars using command-line options?
+
+
+Contributing
+------------
+
+- If you find a new bug, a missing feature, etc. please create a ticket in Isuses.
+- Contributions are very welcome - just open a pull-request.
+
+
+Licence
+=======
+
+MIT
+
+
+Author
+======
+
+Dan Keder <dan.keder@gmail.com>
---
+++ pg_dump_sample.go
@@ -0,0 +1,441 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"os/user"
+	"strconv"
+	"strings"
+
+	"github.com/cbroglie/mustache"
+	flags "github.com/jessevdk/go-flags"
+	"golang.org/x/crypto/ssh/terminal"
+	pg "gopkg.in/pg.v4"
+	yaml "gopkg.in/yaml.v2"
+)
+
+const (
+	BEGIN_DUMP = `
+--
+-- PostgreSQL database dump
+--
+
+BEGIN;
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+SET search_path = public, pg_catalog;
+
+`
+
+	END_DUMP = `
+COMMIT;
+
+--
+-- PostgreSQL database dump complete
+--
+`
+
+	BEGIN_TABLE_DUMP = `
+--
+-- Data for Name: %s; Type: TABLE DATA
+--
+
+COPY %s (%s) FROM stdin;
+`
+
+	END_TABLE_DUMP = `\.
+`
+
+	SQL_CMD_DUMP = "\n%s;\n"
+)
+
+type Options struct {
+	Host         string
+	Port         int
+	Username     string
+	NoPassword   bool
+	ManifestFile string
+	OutputFile   string
+	Database     string
+}
+
+type ManifestItem struct {
+	Table       string   `yaml:"table"`
+	Query       string   `yaml:"query"`
+	Columns     []string `yaml:"columns,flow"`
+	PostActions []string `yaml:"post_actions,flow"`
+}
+
+type Manifest struct {
+	Vars   map[string]string `yaml:"vars"`
+	Tables []ManifestItem    `yaml:"tables"`
+}
+
+type ManifestIterator struct {
+	db       *pg.DB
+	manifest *Manifest
+	todo     map[string]ManifestItem
+	stack    []string
+}
+
+func NewManifestIterator(db *pg.DB, manifest *Manifest) *ManifestIterator {
+	m := ManifestIterator{
+		db,
+		manifest,
+		make(map[string]ManifestItem),
+		make([]string, 0),
+	}
+
+	for _, item := range m.manifest.Tables {
+		m.stack = append(m.stack, item.Table)
+		m.todo[item.Table] = item
+	}
+
+	return &m
+}
+
+func (m *ManifestIterator) Next() (*ManifestItem, error) {
+	if len(m.stack) == 0 {
+		return nil, nil
+	}
+
+	table := m.stack[0]
+	m.stack = m.stack[1:]
+
+	if _, ok := m.todo[table]; !ok {
+		return m.Next()
+	}
+
+	deps, err := getTableDeps(m.db, table)
+	if err != nil {
+		return nil, err
+	}
+
+	todoDeps := make([]string, 0)
+	for _, dep := range deps {
+		if _, ok := m.todo[dep]; ok && table != dep {
+			todoDeps = append(todoDeps, dep)
+		}
+	}
+
+	if len(todoDeps) > 0 {
+		m.stack = append(todoDeps, append([]string{table}, m.stack...)...)
+		return m.Next()
+	}
+
+	result := m.todo[table]
+	delete(m.todo, table)
+
+	return &result, nil
+}
+
+func parseArgs() (*Options, error) {
+	var opts struct {
+		Host         string `short:"h" long:"host" default:"/tmp" default-mask:"local socket" description:"database server host or socket directory"`
+		Port         string `short:"p" long:"port" default:"5432" description:"database server port"`
+		Username     string `short:"U" long:"username" default-mask:"current user" description:"database user name"`
+		NoPassword   bool   `short:"w" long:"no-password" description:"never prompt for password"`
+		ManifestFile string `short:"m" long:"manifest-file" description:"path to manifest file"`
+		OutputFile   string `short:"f" long:"file" description:"path to output file"`
+		Help         bool   `long:"help" description:"show help"`
+	}
+
+	parser := flags.NewParser(&opts, flags.None)
+	parser.Usage = "[options] database"
+
+	args, err := parser.Parse()
+	if err != nil {
+		parser.WriteHelp(os.Stderr)
+		return nil, err
+	}
+
+	if opts.Help {
+		parser.WriteHelp(os.Stdout)
+		os.Exit(0)
+	}
+
+	if len(args) < 1 {
+		parser.WriteHelp(os.Stderr)
+		return nil, fmt.Errorf("required argument `database` not specified")
+	}
+
+	if len(args) > 1 {
+		parser.WriteHelp(os.Stderr)
+		return nil, fmt.Errorf("only one database may be specified at a time")
+	}
+
+	if opts.ManifestFile == "" {
+		parser.WriteHelp(os.Stderr)
+		return nil, fmt.Errorf("required flag `-m, --manifest-file` not specified")
+	}
+
+	if opts.Username == "" {
+		currentUser, err := user.Current()
+		if err != nil {
+			return nil, fmt.Errorf("failed to get current user")
+		}
+		opts.Username = currentUser.Username
+	}
+
+	port, err := strconv.Atoi(opts.Port)
+	if err != nil {
+		parser.WriteHelp(os.Stderr)
+		return nil, fmt.Errorf("port must be a number 0-65535")
+	}
+
+	return &Options{
+		Host:         opts.Host,
+		Port:         port,
+		Username:     opts.Username,
+		NoPassword:   opts.NoPassword,
+		ManifestFile: opts.ManifestFile,
+		OutputFile:   opts.OutputFile,
+		Database:     args[0],
+	}, nil
+}
+
+func connectDB(opts *pg.Options) (*pg.DB, error) {
+	db := pg.Connect(opts)
+	var model []struct {
+		X string
+	}
+	_, err := db.Query(&model, `SELECT 1 AS x`)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
+
+func beginDump(w io.Writer) {
+	fmt.Fprintf(w, BEGIN_DUMP)
+}
+
+func endDump(w io.Writer) {
+	fmt.Fprintf(w, END_DUMP)
+}
+
+func beginTable(w io.Writer, table string, columns []string) {
+	quoted := make([]string, 0)
+	for _, v := range columns {
+		quoted = append(quoted, strconv.Quote(v))
+	}
+	colstr := strings.Join(quoted, ", ")
+	fmt.Fprintf(w, BEGIN_TABLE_DUMP, table, table, colstr)
+}
+
+func endTable(w io.Writer) {
+	fmt.Fprintf(w, END_TABLE_DUMP)
+}
+
+func dumpSqlCmd(w io.Writer, v string) {
+	fmt.Fprintf(w, SQL_CMD_DUMP, v)
+}
+
+func dumpTable(w io.Writer, db *pg.DB, table string) error {
+	sql := fmt.Sprintf(`COPY %s TO STDOUT`, table)
+
+	_, err := db.CopyTo(w, sql)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func readPassword(username string) (string, error) {
+	fmt.Printf("Password for %s: ", username)
+	password, err := terminal.ReadPassword(0)
+	fmt.Print("\n")
+	return string(password), err
+}
+
+func readManifest(r io.Reader) (*Manifest, error) {
+	data, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	manifest := Manifest{}
+	yaml.Unmarshal(data, &manifest)
+
+	return &manifest, nil
+}
+
+func getTableCols(db *pg.DB, table string) ([]string, error) {
+	var model []struct {
+		Colname string
+	}
+	sql := `
+		SELECT attname as colname
+		FROM pg_catalog.pg_attribute
+		WHERE
+			attrelid = ?::regclass
+			AND attnum > 0
+			AND attisdropped = FALSE
+			ORDER BY attnum
+	`
+	_, err := db.Query(&model, sql, table)
+	if err != nil {
+		return nil, err
+	}
+
+	var cols = make([]string, 0)
+	for _, v := range model {
+		cols = append(cols, v.Colname)
+	}
+
+	return cols, nil
+}
+
+func getTableDeps(db *pg.DB, table string) ([]string, error) {
+	var model []struct {
+		Tablename string
+	}
+	sql := `
+		SELECT confrelid::regclass AS tablename
+		FROM pg_catalog.pg_constraint
+		WHERE
+			conrelid = ?::regclass
+			AND contype = 'f'
+	`
+	_, err := db.Query(&model, sql, table)
+	if err != nil {
+		return nil, err
+	}
+
+	var tables = make([]string, 0)
+	for _, v := range model {
+		tables = append(tables, v.Tablename)
+	}
+
+	return tables, nil
+}
+
+func makeDump(db *pg.DB, manifest *Manifest, w io.Writer) error {
+	beginDump(w)
+
+	iterator := NewManifestIterator(db, manifest)
+	for {
+		v, err := iterator.Next()
+		if err != nil {
+			return err
+		}
+		if v == nil {
+			break
+		}
+
+		cols := v.Columns
+		if len(cols) == 0 {
+			cols, err = getTableCols(db, v.Table)
+			if err != nil {
+				return err
+			}
+		}
+
+		beginTable(w, v.Table, cols)
+		if v.Query == "" {
+			err := dumpTable(w, db, v.Table)
+			if err != nil {
+				return err
+			}
+		} else {
+			query, err := mustache.Render(v.Query, manifest.Vars)
+			if err != nil {
+				return err
+			}
+
+			err = dumpTable(w, db, fmt.Sprintf("(%s)", query))
+			if err != nil {
+				return err
+			}
+		}
+		endTable(w)
+
+		for _, sql := range v.PostActions {
+			dumpSqlCmd(w, sql)
+		}
+	}
+
+	endDump(w)
+
+	return nil
+}
+
+func main() {
+	opts, err := parseArgs()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Open manifest file
+	manifestFile, err := os.Open(opts.ManifestFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Read manifest
+	manifest, err := readManifest(manifestFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Open output file
+	output := os.Stdout
+	if opts.OutputFile != "" {
+		output, err = os.OpenFile(opts.OutputFile, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	// Connect to the DB
+	db, err := connectDB(&pg.Options{
+		Addr:     fmt.Sprintf("%s:%d", opts.Host, opts.Port),
+		Database: opts.Database,
+		User:     opts.Username,
+		SSL:      false,
+	})
+	if err != nil {
+		password := ""
+		if !opts.NoPassword {
+			// Read database password
+			password, err = readPassword(opts.Username)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+				os.Exit(1)
+			}
+		}
+
+		// Try again, this time with password
+		db, err = connectDB(&pg.Options{
+			Addr:     fmt.Sprintf("%s:%d", opts.Host, opts.Port),
+			Database: opts.Database,
+			User:     opts.Username,
+			Password: password,
+			SSL:      false,
+		})
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+			os.Exit(1)
+		}
+	}
+
+	// Make the dump
+	err = makeDump(db, manifest, output)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
+}
